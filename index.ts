dd5f1a0c8245079ff9a859544bfe211acc716b32
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-07-25 16:17:08 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,3 +5,7 @@ export { default as CsvWriter } from '@/writers/CsvWriter.js';
 /* * */
 
 export { default as MongoDbWriter } from '@/writers/MongoDbWriter.js';
+
+/* * */
+
+export { default as PostgresWriter } from '@/writers/PostgresWriter.js';
